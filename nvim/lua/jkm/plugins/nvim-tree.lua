76a2f188cfce70a4ef60df471620f2d2d315e861
Joakim Edvardsen <joakimedvardsen2000@gmail.com>
2024-11-28 20:31:02 +0100

    nvim: fresh start

---
+++ nvim/lua/jkm/plugins/nvim-tree.lua
@@ -0,0 +1,31 @@
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+  return
+end
+
+-- recommended settings from nvim-tree documentation
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- change color explorer arrows
+vim.cmd([[ highlight NvimTreeIdentMarker guifg=#3FC5FF ]])
+
+nvimtree.setup({
+  renderer = {
+    icons = {
+      glyphs = {
+        folder = {
+          arrow_closed = ">",
+          arrow_open = "v"
+        },
+      },
+    },
+  },
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = false
+      },
+    },
+  },
+})
