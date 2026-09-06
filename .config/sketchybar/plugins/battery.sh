890ede212fca0d72e27bc661b7dce43fd4f7f63d
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2026-09-06 15:39:05 +0200

    fix(toolbar): battery icon

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -10,19 +10,29 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON="󰁹"
   ;;
-  [6-8][0-9]) ICON=""
+  8[0-9]) ICON="󰂂"
   ;;
-  [3-5][0-9]) ICON=""
+  7[0-9]) ICON="󰂁"
   ;;
-  [1-2][0-9]) ICON=""
+  6[0-9]) ICON="󰂀"
   ;;
-  *) ICON=""
+  5[0-9]) ICON="󰁿"
+  ;;
+  4[0-9]) ICON="󰁾"
+  ;;
+  3[0-9]) ICON="󰁽"
+  ;;
+  2[0-9]) ICON="󰁼"
+  ;;
+  1[0-9]) ICON="󰁻"
+  ;;
+  *) ICON="󰁺"
 esac
 
 if [[ "$CHARGING" != "" ]]; then
-  ICON=""
+  ICON="󰂄"
   COLOR=$GREEN
 elif [ "$PERCENTAGE" -le 20 ]; then
   COLOR=$RED
